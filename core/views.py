3d4e99c2d38cd6ff459987147065c103ddc64ce1
fabinhodesigns <freitasfabiano08@gmail.com>
2025-06-07 20:55:35 -0300

    Ajustes

--- core/views.py
+++ core/views.py
@@ -67,21 +67,22 @@ def login_view(request):
 
 @login_required(login_url='/login/')
 def suas_empresas(request):
-    empresas = Empresa.objects.filter(user=request.user).order_by('-data_cadastro')
+    print("Tteste")
+    # empresas = Empresa.objects.filter(user=request.user).order_by('-data_cadastro')
 
-    paginator = Paginator(empresas, 6) 
-    page_number = request.GET.get('page') or 1
-    page_obj = paginator.get_page(page_number)
+    # paginator = Paginator(empresas, 6) 
+    # page_number = request.GET.get('page') or 1
+    # page_obj = paginator.get_page(page_number)
 
-    if request.is_ajax():
-        html = render_to_string('core/partials/empresas_cards.html', {'page_obj': page_obj})
-        return JsonResponse({
-            'html': html,
-            'has_next': page_obj.has_next(),
-            'next_page_number': page_obj.next_page_number() if page_obj.has_next() else None,
-        })
+    # if request.is_ajax():
+    #     html = render_to_string('core/partials/empresas_cards.html', {'page_obj': page_obj})
+    #     return JsonResponse({
+    #         'html': html,
+    #         'has_next': page_obj.has_next(),
+    #         'next_page_number': page_obj.next_page_number() if page_obj.has_next() else None,
+    #     })
 
-    return render(request, 'core/suas_empresas.html', {'page_obj': page_obj})
+    # return render(request, 'core/suas_empresas.html', {'page_obj': page_obj})
 
 @login_required(login_url='/login/')
 def cadastrar_empresa(request):
