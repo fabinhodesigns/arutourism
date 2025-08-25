4e00f931f0c007e96de7ea4020fe39b593375edd
Fabiano Freitas <freitasfabiano08@gmail.com>
2025-08-25 20:51:59 -0300

    Adjustments

--- arutourism/settings.py
+++ arutourism/settings.py
@@ -73,8 +73,7 @@ WSGI_APPLICATION = 'arutourism.wsgi.application'
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR/'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=True
+        conn_max_age=600
     )
 }
 
